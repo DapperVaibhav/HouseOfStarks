847a9ac43022d91ea9de94eec873d40c81794593
vaibhav <vaibhav@DESKTOP-9MFP4A2>
2020-06-01 01:11:41 +0530

    Change in code 01/06/2020

--- Google/src/g/GL.java
+++ Google/src/g/GL.java
@@ -12,21 +12,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GL {
     static WebDriver driver;
-    static Logger Log = Logger.getLogger(GL.class.getName());
 	public static void main(String[] args) throws InterruptedException {
 		
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\vaibhav\\Desktop\\Vaihav imp data\\ChromeDriver\\ChromeDriver_31052020\\chromedriver_win32 (1)\\chromedriver.exe");
-    Log.info("Chrome Path set");
+    
 	driver=new ChromeDriver();
-	Log.info("ChromeDriver initiated");
+	
     driver.get("https://www.makemytrip.com/");
     driver.manage().window().maximize();
-    Log.info("Website Launched");
+    
     Thread.sleep(2500); 
     System.out.println("Done");
     driver.close();
-    Log.info("Chrome closed");
-
+    
 	}
 
 }
