0be359d2883ddb7c4e38c4264a23e23b236cc791
vaibhav <vaibhav@DESKTOP-9MFP4A2>
2020-06-01 01:07:42 +0530

    Code Update 01/06/2020

--- Google/src/g/GL.java
+++ Google/src/g/GL.java
@@ -1,28 +1,34 @@
 package g;
 
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Logger;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GL {
     static WebDriver driver;
+    static Logger Log = Logger.getLogger(GL.class.getName());
 	public static void main(String[] args) throws InterruptedException {
 		
-	System.setProperty("webdriver.chrome.driver", "C:\\Users\\vaibhav\\Desktop\\Vaihav imp data\\ChromeDriver\\chromedriver_win32 (1)\\chromedriver.exe");
-    driver=new ChromeDriver();
-    driver.get("https://www.google.com/");
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\vaibhav\\Desktop\\Vaihav imp data\\ChromeDriver\\ChromeDriver_31052020\\chromedriver_win32 (1)\\chromedriver.exe");
+    Log.info("Chrome Path set");
+	driver=new ChromeDriver();
+	Log.info("ChromeDriver initiated");
+    driver.get("https://www.makemytrip.com/");
     driver.manage().window().maximize();
-    Thread.sleep(2500);
-    
+    Log.info("Website Launched");
+    Thread.sleep(2500); 
     System.out.println("Done");
-    
-    driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Vaibhav Tanavde");
-    
-    System.out.println("Done");
-    
     driver.close();
+    Log.info("Chrome closed");
 
 	}
 
 }
-;
+
+//
